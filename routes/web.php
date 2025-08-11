bd938e5e57ddbf71db8a63d3dd3be31aa5b10849
dareb21 <qtin10036@gmail.com>
2025-08-11 15:47:33 -0600

    sin middlewares

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\GeneralController
-;
+use App\Http\Controllers\GeneralController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LaywerController;
+
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+    Route::get("/dashboard",[GeneralController::class, 'dashboard']);
+    Route::get("/dirs",[GeneralController::class, 'showDirs']);
+    Route::get("/thisDir/{thisDir}",[GeneralController::class, 'showThisDir']);
+    Route::post("/makeDir",[GeneralController::class, 'makeDir']);
+    Route::post("/uploadDoc/{thisDir}",[GeneralController::class, 'uploadDoc']);
+    Route::get("/downloadDoc/{thisDoc}",[GeneralController::class, 'downloadDoc']);
+    Route::post("/downloadRequest/{thisDoc}",[GeneralController::class, 'downloadRequest']);
 
-Route::get('/auth/redirect',[LoginController::class,"login"])->name("login"); 
-Route::get("/auth/google/callback",[LoginController::class,"handdleCallBack"]);
+        Route::get('/users', [AdminController::class, 'showUsers']);
+        Route::post('/newUser', [AdminController::class, 'newUser']);
+        Route::patch('/banThisUser/{userId}', [AdminController::class, 'banThisUser']);
+        Route::patch('/unBanThisUser/{userId}', [AdminController::class, 'unBanThisUser']);
+        //ver logs
+        //Editar
 
- Route::post("/uploadDoc/{thisDir}",[GeneralController::class, 'uploadDoc']);
-Route::get("/downloadDoc/{thisDoc}",[GeneralController::class, 'downloadDoc']);
-  
- route::get("/home",[GeneralController::class, 'home']);
+        Route::get('/seeRequest', [LaywerController::class, 'seeRequest']);   
+        Route::patch('/replyRequest/{thisRequest}', [LaywerController::class, 'replyRequest']);
+        Route::delete('/deleteDoc/{thisDoc}', [LaywerController::class, 'deleteDoc']);
+        Route::delete('/deleteDir/{thisDir}', [LaywerController::class, 'deleteDir']);
+        Route::get('/recycle', [LaywerController::class, 'recycleCan']);
+        Route::patch('/restore/{thisDoc}', [LaywerController::class, 'restoreDoc']);
+        Route::patch('/restoreDir/{thisDir}', [LaywerController::class, 'restoreDir']);
+        Route::patch('/finished/{thisDir}', [LaywerController::class, 'finishThisCase']);
+

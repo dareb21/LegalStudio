72e4aa396bd6890ec3bfa1ac58c4e54cb5179624
dareb21 <qtin10036@gmail.com>
2025-09-10 11:29:18 -0600

    seeders de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Assistant;
-use App\Models\Document;
-use App\Models\Laywer;
-use App\Models\Record;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,72 +16,117 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::create([
-    'name' => 'Juan Gabriel Garcia',
-    'email' => 'juan.gabriel@example.com',
-    'phone' => '50490000001',
+User::create([
+    'name' => 'Juan José Martinez Espinal',
+    'email' => 'jj_martinezespinal@estudiolegalmc.com',
+    'phone' => '99556342',
     'birthday' => '1985-03-15',
-    'role' => 'asistente',
+    'role' => 'Abogado',
 ]);
 
 User::create([
-    'name' => 'Ana María Torres',
-    'email' => 'ana.torres@example.com',
-    'phone' => '50490000002',
+    'name' => 'Raul Edgardo Chinchilla Aguilar',
+    'email' => 'Raul.ed.chinchilla@estudiolegalmc.com',
+    'phone' => '98908152',
     'birthday' => '1990-06-22',
     'role' => 'Abogado',
 ]);
 
-
 User::create([
-    "email"=> 'fernandossrc@gmail.com',
-    "name"=>'Luis Fernando Fuentes Tabora',
-    "phone"=>"32310952",
-    "birthday"=>"1834-03-12",
-    "role"=>"Abogado"
-]);
-User::create([
-    "email"=>'3190019@usap.edu',
-    'name'=>'Luis fernando Asistente',
-    'phone'=>'32323232',
-    'birthday'=>'2002-04-12',
-    'role'=>'Asistente'
+    'name' => 'Carol Yolanda Martinez Mejia',
+    'email' => 'Martinezcastellon@estudiolegalmc.com',
+    'phone' => '32378650',
+    'birthday' => '1990-06-22',
+    'role' => 'Abogado',
 ]);
 
 User::create([
-    "email"=> 'Raul.ed.chinchilla@bufete-chinchillapaz.com',
-    "name"=>'Raul Edgardo Chincilla Aguilar',
-    "phone"=>"98908152",
+    'name' => 'Fany Griselda Castellón Vallecillo',
+    'email' => 'Fanycastellon@estudiolegalmc.com',
+    'phone' => '99264747',
+    'birthday' => '1990-06-22',
+    'role' => 'Abogado',
+]);
+
+User::create([
+    'name' => 'José Carlos Martínez Castellón',
+    'email' => 'Jcmartinez@estudiolegalmc.com',
+    'phone' => '95277216',
+    'birthday' => '1990-06-22',
+    'role' => 'Abogado',
+]);
+
+User::create([
+    'name' => 'Carlos Rene Antunez Lazo',
+    'email' => 'Carlosantunez07@gmail.com',
+    'phone' => '33899012',
+    'birthday' => '1990-06-22',
+    'role' => 'Asistente',
+]);
+
+User::create([
+    'name' => 'Carlos David Carranza Martinez',
+    'email' => 'Abgcarloscarranza@gmail.com',
+    'phone' => '33955834',
+    'birthday' => '1990-06-22',
+    'role' => 'Asistente',
+]);
+
+User::create([
+    'name' => 'Verónica Zamora',
+    'email' => 'Vzamora1978@gmail.com',
+    'phone' => '99043091',
+    'birthday' => '1990-06-22',
+    'role' => 'Asistente',
+]);
+
+User::create([
+    "email"=> 'palma.chn15@gmail.com',
+    "name"=>'Carlos Admin',
+    "phone"=>"21212121",
+    "birthday"=>"2002-12-20",
+    "role"=>"Admin"
+]);
+
+User::create([
+    "email"=> '2210088@usap.edu',
+    "name"=>'Carlos Abogado',
+    "phone"=>"45677623",
     "birthday"=>"2002-12-20",
     "role"=>"Abogado"
 ]);
 
 User::create([
-    "email"=> 'lourdespaz@bufete-chinchillapaz.com',
-    "name"=>'Lourdes Delmar Paz Paz',
-    "phone"=>"94662955",
+    "email"=> 'practicas25.carlospalma@usap.edu',
+    "name"=>'Carlos Asistente',
+    "phone"=>"22786501",
     "birthday"=>"2002-12-20",
     "role"=>"Asistente"
 ]);
 
 
 User::create([
-    "email"=> 'palma.chn15@gmail.com',
-    "name"=>'Carlos Palma',
-    "phone"=>"21212121",
-    "birthday"=>"2002-12-20",
-    "role"=>"Admin"
-]);
-
-
-User::create([
-    "email"=> 'practicas25.luisfuentes@usap.edu',
-    "name"=>'Luis el fucker',
+    "email"=> 'kindleluis17@gmail.com',
+    "name"=>'Luis ADMIN',
     "phone"=>"90909090",
     "birthday"=>"2002-12-20",
     "role"=>"Admin"
 ]);
 
+User::create([
+    "email"=> 'fernandossrc@gmail.com',
+    "name"=>'Luis Abogado',
+    "phone"=>"32310952",
+    "birthday"=>"1834-03-12",
+    "role"=>"Abogado"
+]);
+User::create([
+    "email"=>'3190019@usap.edu',
+    'name'=>'Luis Asistente',
+    'phone'=>'32323232',
+    'birthday'=>'2002-04-12',
+    'role'=>'Asistente'
+]);
 
 
     }
